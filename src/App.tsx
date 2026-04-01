d38fb976baa41d9da483c983c8bc9058f2c8438b
sarthak <sarthak.chowdhry@hotmail.com>
2026-04-01 23:52:06 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+    <div className="flex size-full min-h-svh p-6">
+      <div className="flex w-full min-w-0 flex-col items-center justify-center gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          <Button className="mt-2" variant={"default"}>
+            Button
+          </Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
